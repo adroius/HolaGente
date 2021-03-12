5ed1e2e537444ea99393c21bd3ba7e367a6086c3
adroius <74370106+adroius@users.noreply.github.com>
2021-03-12 18:42:40 +0100

    Update Main.java

--- src/Practica/Main.java
+++ src/Practica/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Hola soy adrian y tu?");
-
+        System.out.println("Prueba desde el buscador, al ser publico");
     }
 }
