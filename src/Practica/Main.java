a08891a81078dd86f9ba50d00cff654e46acdcce
adrianPG <adrianpenaga@gmail.com>
2021-03-12 18:44:04 +0100

    pruebaUno

--- src/Practica/Main.java
+++ src/Practica/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Hola soy adrian y tu?");
-        System.out.println("Prueba desde el buscador, al ser publico");
+
+        System.out.println("Entonces ahora esto no aparecera");
     }
 }
